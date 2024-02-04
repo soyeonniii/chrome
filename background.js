c725955bac2d8917a0d56a945ef201aad5933d6f
soyeonniii <155460788+soyeonniii@users.noreply.github.com>
2024-02-04 23:58:55 +0900

    background.js

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpg", "1.jpg", "016.jpg", "017.jpg"];
+const images = ["img/0.jpg", "img/1.jpg", "img/016.jpg", "img/017.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
